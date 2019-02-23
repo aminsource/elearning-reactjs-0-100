14a8bd5f6b1b08d56d8c1541489a555e6b75ffa0
Hooman Amini <amini.hooman@gmail.com>
2019-02-23 11:51:03 +0330

    lesson7

---
+++ 0-100app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>۰-100 React App</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
