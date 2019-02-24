02d52aa960e71bd276e3405c92e9283be8cc12b5
Hooman Amini <amini.hooman@gmail.com>
2019-02-24 13:04:42 +0330

    lesson9

--- 0-100app/src/App.js
+++ 0-100app/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Ractor from "./Ractor";
 class App extends Component {
+  state = {
+    ractors: [
+      { name: "ali", age: "30", id: "1" },
+      { name: "hassan", age: "45", id: "2" },
+      { name: "sina", age: "25", id: "3" }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Ractor App</h1>
-        <Ractor />
+        <Ractor ractors={this.state.ractors} />
       </div>
     );
   }
--- 0-100app/src/Ractor.js
+++ 0-100app/src/Ractor.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 
 class Ractor extends Component {
   render() {
-    return (
-      <div className="ractor">
-        <div>Name: Ali</div>
-        <div>Age:30</div>
-      </div>
-    );
+    const { ractors } = this.props;
+    const ractorsList = ractors.map(ractor => {
+      return (
+        <div className="ractor" key={ractor.id}>
+          <div>Name: {ractor.name}</div>
+          <div>Age:{ractor.age}</div>
+        </div>
+      );
+    });
+    return <div className="ractorList">{ractorsList}</div>;
   }
 }
 export default Ractor;
