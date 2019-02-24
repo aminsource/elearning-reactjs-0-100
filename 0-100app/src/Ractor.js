7bc41a2d85d021c43d125678f299acf319dc8df4
Hooman Amini <amini.hooman@gmail.com>
2019-02-24 20:22:24 +0330

    lesson10

--- 0-100app/src/Ractor.js
+++ 0-100app/src/Ractor.js
@@ -1,17 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ractor extends Component {
-  render() {
-    const { ractors } = this.props;
-    const ractorsList = ractors.map(ractor => {
-      return (
-        <div className="ractor" key={ractor.id}>
-          <div>Name: {ractor.name}</div>
-          <div>Age:{ractor.age}</div>
-        </div>
-      );
-    });
-    return <div className="ractorList">{ractorsList}</div>;
-  }
-}
+const Ractor = ({ ractors }) => {
+  const ractorsList = ractors.map(ractor => {
+    return (
+      <div className="ractor" key={ractor.id}>
+        <div>Name: {ractor.name}</div>
+        <div>Age:{ractor.age}</div>
+      </div>
+    );
+  });
+  return <div className="ractorList">{ractorsList}</div>;
+};
 export default Ractor;
