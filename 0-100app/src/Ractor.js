6c58d79fabcee7d466e36f74bb7180e0ff1405c0
Hooman Amini <amini.hooman@gmail.com>
2019-02-25 09:09:22 +0330

    lesson11

--- 0-100app/src/Ractor.js
+++ 0-100app/src/Ractor.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 const Ractor = ({ ractors }) => {
+  // const ractorsList = ractors.map(ractor => {
+  //   if (ractor.age > 30) {
+  //     return (
+  //       <div className="ractor" key={ractor.id}>
+  //         <div>Name: {ractor.name}</div>
+  //         <div>Age:{ractor.age}</div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // });
   const ractorsList = ractors.map(ractor => {
-    return (
+    return ractor.age > 25 ? (
       <div className="ractor" key={ractor.id}>
         <div>Name: {ractor.name}</div>
         <div>Age:{ractor.age}</div>
       </div>
-    );
+    ) : null;
   });
   return <div className="ractorList">{ractorsList}</div>;
 };
